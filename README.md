e467e2c77e757ddf1c0c577f70751b9e12f01861
Peter Lesslie <pclesslie@gmail.com>
2014-05-02 00:40:17 -0500

    Add Report

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ eecs678_mmap
 ============
 
 Memory mapping
+
+1. The time required to copy the file using read_write varies with the size
+of the buffer specified. Smaller buffer sizes take longer. The time required
+for memmap varies much less regardless of how you perform the copy. Discuss
+why this is, and in your discussion consider the influence of: (1) the overhead
+of the read() and write() system calls and (2) the number of times the data is 
+copied under the two methods.
+
+First, with a smaller buffer size, the read() and write() system calls have to
+called more times.  These system calls have quite a bit of overhead because
+they have to switch to kernel mode and back.  Second, when mmap is used it
+allows the operating system to save a lot of physical pages of memory by sharing
+the memory mapped regions.  In the read_write program the data has to be copied
+to the buffer during the read() call, then re-copied during the write() call.
+In the memmap program the data is just copied from the file to the other file,
+both are in kernel space.
+
+2. When you use the read_write command as supplied, the size of the copied file
+varies with the size of the buffer specified. When you use the memmap command
+implemented the size of the source and destination files will watch exactly.
+This is because there is a mistake in the read_write code. What is the mistake,
+and how can it be corrected?
+
+Currently, read_write will write an integer multiple of bufsz to the new file so
+it might (and probably will) write more data than there actually is in the input
+file.
---
+++ memmap.c
@@ -0,0 +1,108 @@
+/*
+ * Example of using mmap. Taken from Advanced Programming in the Unix
+ * Environment by Richard Stevens.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h> /* mmap() is defined in this header */
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>  /* memcpy */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void err_quit (const char * mesg)
+{
+  printf ("%s\n", mesg);
+  exit(1);
+}
+
+void err_sys (const char * mesg)
+{
+  perror(mesg);
+  exit(errno);
+}
+
+int main (int argc, char *argv[])
+{
+  int fdin, fdout, i;
+  char *src, *dst, buf[256];
+  struct stat statbuf;
+
+  src = dst = NULL;
+
+  if (argc != 3)
+    err_quit ("usage: memmap <fromfile> <tofile>");
+
+  /* 
+   * open the input file 
+   */
+  if ((fdin = open (argv[1], O_RDONLY)) < 0) {
+    sprintf(buf, "can't open %s for reading", argv[1]);
+    perror(buf);
+    exit(errno);
+  }
+
+  /* 
+   * open/create the output file 
+   */
+  if ((fdout = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) {
+    sprintf (buf, "can't create %s for writing", argv[2]);
+    perror(buf);
+    exit(errno);
+  }
+
+  /* 
+   * 1. find size of input file 
+   */
+  if (fstat (fdin, &statbuf) < 0) {
+    err_sys ("Unable to stat file");
+  }
+
+  /* 
+   * 2. go to the location corresponding to the last byte 
+   */
+  if (lseek (fdout, statbuf.st_size, SEEK_SET) < 0) {
+    err_sys ("lseek");
+  }
+
+  /* 
+   * 3. write a dummy byte at the last location 
+   */
+  if (write (fdout, "a", 1) < 0) {
+    err_sys ("write");
+  }
+
+  /* 
+   * 4. mmap the input file 
+   */
+  //  void *mmap( void *start, size_t length, int prot, int flags, int fd, off_t offset );
+  if ((src = mmap (NULL, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0 )) < 0) {
+    err_sys ("mmap: input");
+  }
+
+  /* 
+   * 5. mmap the output file 
+   */
+  if ((dst = mmap (NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0)) < 0) {
+    err_sys ("mmap: output");
+  }
+
+
+  /* 
+   * 6. copy the input file to the output file 
+   */
+    /* Memory can be dereferenced using the * operator in C.  This line
+     * stores what is in the memory location pointed to by src into
+     * the memory location pointed to by dest.
+     */
+  //    *dst = *src;
+  memcpy( dst, src, statbuf.st_size );
+
+  close( fdin );
+  close( fdout );
+} 
+
+
